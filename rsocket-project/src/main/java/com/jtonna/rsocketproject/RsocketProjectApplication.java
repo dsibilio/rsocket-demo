51e441a5e8efbdafa8760eea713fb029454cc6a0
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-12-06 23:31:08 -0800

    Initialized project

---
+++ rsocket-project/src/main/java/com/jtonna/rsocketproject/RsocketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jtonna.rsocketproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketProjectApplication.class, args);
+    }
+
+}
---
+++ rsocket-project/src/test/java/com/jtonna/rsocketproject/RsocketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtonna.rsocketproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
